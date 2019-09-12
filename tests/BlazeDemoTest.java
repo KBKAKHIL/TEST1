fc2d5b4557498abb3614a4430eb237c69dcfc88c
macstudent <macstudent@10.10.152.0>
2019-09-12 12:56:38 -0400

    Added 3 Test cases in BlazeDemo

--- tests/BlazeDemoTest.java
+++ tests/BlazeDemoTest.java
@@ -3,16 +3,21 @@ import static org.junit.Assert.fail;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BlazeDemoTest {
- //  WebDriver driver;
+   WebDriver driver;
 	@Before
 	public void setUp() throws Exception {
 		System.setProperty("webdriver.chrome.driver",
 				"/Users/macstudent/Desktop/chromedriver");
-	//	driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		String baseUrl = "http://blazedemo.com/";
-	//	driver.get(baseUrl);
+		driver.get(baseUrl);
+		Thread.sleep(4000);
 
 	}
 
@@ -21,8 +26,28 @@ public class BlazeDemoTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCase1() throws InterruptedException {
+		WebElement check = driver.findElement(By.name("fromPort"));
+		check.click();
+		driver.close();
+		Thread.sleep(4000);
+		
+		
 	}
+	@Test
+	public void testCase2() throws InterruptedException {
+		
+		WebElement details = driver.findElement(By.className("btn btn-primary"));
+		details.click();
+		driver.close();
+		Thread.sleep(4000);
+	}
+	
+	@Test
+	public void testCase3() throws InterruptedException {
+		
+		
+	}
+	
 
 }
